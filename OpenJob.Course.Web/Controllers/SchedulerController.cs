884480e6381ee251f5222165be1efe5b41f3539d
Emiliano Paggio <emiliano.paggio@gmail.com>
2019-06-20 13:11:42 +0200

    prova1

---
+++ OpenJob.Course.Web/Controllers/SchedulerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OpenJob.Course.Web.Controllers
+{
+    public class SchedulerController : Controller
+    {
+        // GET: Scheduler
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
