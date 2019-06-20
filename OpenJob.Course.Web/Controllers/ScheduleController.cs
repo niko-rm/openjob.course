82dbdee3d34e6b623293045e19ee4f8cfa1a6c8a
Andrei <andreibalan258@outlook.it>
2019-06-20 17:13:49 +0200

    commit per lo schedule con riempimento dinamico della lista

--- OpenJob.Course.Web/Controllers/ScheduleController.cs
+++ OpenJob.Course.Web/Controllers/ScheduleController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using OpenJob.Course.DbCourse.Entities;
+using OpenJob.Course.Web.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,10 +11,31 @@ namespace OpenJob.Course.Api.Controllers
 {
     public class ScheduleController : Controller
     {
+        private DbCourseCtx db = new DbCourseCtx();
+
         // GET: Schedule
-        public ActionResult Schedule()
+        public async Task<ActionResult> Index()
         {
-            return View();
+            var ScheduleViewModels = new OpenJob.Course.Web.Models.ScheduleViewModels();
+            var listStudentDTO = await Student.GetAll(db);
+            var listLessonDTO = await Lesson.GetAll(db);
+            var listClassroomDTO = await ClassRoom.GetAll(db);
+
+            //logica per il caricamento
+            ScheduleViewModels.listStudents = listStudentDTO.Select(x => new StudentViewModels() { IdStudent = x.IdStudent, Name = x.Name, SurName = x.SurName }).ToList();
+            ScheduleViewModels.listLessons = listLessonDTO.Select(x => new LessonViewModels() { IdLesson = x.IdLesson, Name = x.Name}).ToList();
+            ScheduleViewModels.listClassroom = listClassroomDTO.Select(x => new ClassRoomViewModels() { IdClassRoom = x.IdClassRoom, Name = x.Name }).ToList();
+            return View(ScheduleViewModels);
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
---
+++ OpenJob.Course.Web/Models/ScheduleViewModels.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OpenJob.Course.Web.Models
+{
+    public class ScheduleViewModels
+    {
+        public List<StudentViewModels> listStudents { get; set; }
+        public List<ClassRoomViewModels> listClassroom { get; set; }
+        public List<LessonViewModels> listLessons { get; set; }
+
+
+    }
+}
