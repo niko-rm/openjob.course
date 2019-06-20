59e84fda04d1d050b927c580dd1acd30ea216c60
Jasminder91 <jassi5991@gmail.com>
2019-06-20 16:00:24 +0200

    Added LogDefinition Class in view model and added LogDefinitionDTO

---
+++ OpenJob.Course.Web/Models/LogDefinitionViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using OpenJob.Course.DTOs;
+using System.Linq;
+using System.Web;
+
+namespace OpenJob.Course.Web.Models
+{
+    public class LogDefinitionViewModel: LogDefinitionDTO
+    {
+    }
+}
